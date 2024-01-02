9cd009a5a96bb70ddd9115d9f0cd0f9603a836a7
victoriakapelush <victoriakapelush@gmail.com>
2024-01-02 02:22:25 -0500

    change app route

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,27 +5,27 @@ export default function Navigation() {
     return(
         <div className="nav-container flex-row">
             <NavComponent 
-            href="hair"
+            
             src="./hairNav.jpg"
             div="Hair"
             />
             <NavComponent 
-            href="bodyandbath"
+            
             src="./body-nav.jpg"
             div="Body and Bath"
             />
             <NavComponent 
-            href="face"
+            
             src="./face-nav.jpg"
             div="Face"
             />
             <NavComponent 
-            href="perfumery"
+            
             src="./perfumery-nav.jpg"
             div="Perfumery"
             />
             <NavComponent 
-            href="makeup"
+            
             src="./makeup-nav.jpg"
             div="Makeup"
             />
