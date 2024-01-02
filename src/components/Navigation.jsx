33963ce87209ce9862fb72079902beb8cf26e234
victoriakapelush <victoriakapelush@gmail.com>
2024-01-02 02:32:54 -0500

    change app route

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,12 +5,30 @@ export default function Navigation() {
     return(
         <div className="nav-container flex-row">
             <NavComponent 
-            href="/hair"
+            href="https://victoriakapelush.com"
             src="./hairNav.jpg"
             div="Hair"
             />
-
-            
+            <NavComponent 
+            href="/bodyandbath"
+            src="./body-nav.jpg"
+            div="Body and Bath"
+            />
+            <NavComponent 
+            href="/face"
+            src="./face-nav.jpg"
+            div="Face"
+            />
+            <NavComponent 
+            href="/perfumery"
+            src="./perfumery-nav.jpg"
+            div="Perfumery"
+            />
+            <NavComponent 
+            href="/makeup"
+            src="./makeup-nav.jpg"
+            div="Makeup"
+            />
         </div>
     )
 }
